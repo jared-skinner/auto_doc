88db218cefc7d90c22c0131055caba962222ebdd
Jared Skinner <jaredds66@gmail.com>
2016-11-01 11:33:22 -0500

    added some options to the python file

--- update_md.py
+++ update_md.py
@@ -5,6 +5,7 @@ from subprocess import call
 import time
 import markdown2
 import re
+import sys
 
 root_dir = os.path.dirname(os.path.realpath(__file__))
 md_dir = root_dir + '/md/'
@@ -39,11 +40,70 @@ def update_file(file_name):
     out = header + nav_html + markdown + footer
     output.write(out)
 
-while True:
-    time.sleep(1)
+
+def usage():
+    print("\nDocumentation Generator\n")
+    print("python update_md.py [Options]\n")
+    print("Options:\n")
+    print(" -h                         Print this help message")
+    print(" -v                         Print version information")
+    print(" -f <file name>             update specified file\n")
+    print(" -a                         Update all files\n")
+    print(" If no option is given this script will continually check for updates")
+    print(" the the md directory and will update the html files accordingly\n")
+
+
+def version():
+    print("\nDocumentation Generator       Version 0.0.1.0\n")
+
+
+merge_version = ""
+assign = False
+merge = False
+documentation = False
+users_guide = False
+test_procedures = False
+config_guide = False
+design_description = False
+issue = ""
+
+argc = len(sys.argv)
+
+all = False
+file = False
+file_name = ""
+if len(sys.argv) > 1:
+    for index, arg in enumerate(sys.argv):
+        if arg == '-h':
+            usage()
+            exit()
+
+        if arg == '-v':
+            version()
+            exit()
+
+        if arg == '-a':
+            all = True
+
+        if arg == '-f':
+            file = True
+            file_name = sys.argv[index + 1]
+
+if all:
+    print("Updating all html")
     for root, dirs, filenames in os.walk(md_dir):
         for file in filenames:
-            if os.path.getmtime(md_dir + file) > current_time:
-                print(file + ": updating html")
+            if re.search("md$", file):
                 update_file(file)
-        current_time = time.time()
+elif file:
+    update_file(file_name)
+else:
+    while True:
+        time.sleep(1)
+        for root, dirs, filenames in os.walk(md_dir):
+            for file in filenames:
+                if os.path.getmtime(md_dir + file) > current_time:
+                    if re.search("md$", file):
+                        print(file + ": updating html")
+                        update_file(file)
+            current_time = time.time()
